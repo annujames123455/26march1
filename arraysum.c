0a2c3a7207fba95c8cca652c21bb269573f965de
annujames <annujames2028@cs.sjcetpalai.ac.in>
2025-03-26 16:01:59 +0530

    Add files via upload

--- arraysum.c
+++ arraysum.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
+int array_sum(int array[],int limit){
+int sum=0;
+  for (int i=0;i<limit;i++){
+    sum=sum+array[i];
+  }
+   return sum;
+} 
 int main(){
-  int i,limit,sum=0;
+int i,limit,sum=0;
   printf("\nEnter the number of  elements ");
   scanf("%d",&limit);
   int array[limit];
   printf("\nEnter the  elements ");
-  for (i=0;i<limit;i++){
+  for (int i=0;i<limit;i++){
+  
     scanf("%d",&array[i]);
-    sum=sum+array[i];
-    
-   }
-   printf("the sum of elements is%d",sum);
-return 0;
-}
+  }
+  sum=array_sum(array,limit);
+  printf("the sum of elements is%d",sum);
+  }
